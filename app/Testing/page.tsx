7db2e87a5f98f1b9825c97aa5f873a20412dd4e4
Depthabdre <abdellaabdre@gmail.com>
2025-04-26 15:18:52 +0300

    Testing Page restored

--- app/Testing/page.tsx
+++ app/Testing/page.tsx
@@ -1,15 +1,8 @@
-import sql  from "@/lib/drizzle";
-import createAuthTables from "@/lib/table";
-import {createTables} from "@/lib/table";
 
-export default async function DbTester() {
-  try {
-    await createAuthTables();
-    console.log('🗑️ Auth Tables Created!');
-    await createTables();
-    console.log('🗑️ Focus Session Tables Created!');
-    
-  } catch (error) {
-    console.error("Error:", error);
-  }
+
+export default  function Tester() {
+  return (
+    <div> This is Testing purpose Only</div>
+  );
+
 }
